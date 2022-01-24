b8b42f1072c73a52338e9c7cf48abda230567b62
Victor Lopez <victor.98.javier@gmail.com>
2022-01-24 00:21:30 -0500

    Constructor done

--- contracts/myepicgame.sol
+++ contracts/myepicgame.sol
@@ -4,8 +4,40 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
+
 contract MyEpicGame {
-  constructor() {
-    console.log("THIS IS MY GAME CONTRACT. NICE.");
+
+  struct CharacterAttributes {
+    uint characterIndex;
+    string name;
+    string imageURI;
+    uint32 hp;
+    uint32 maxHp;
+    uint32 attackDamage;
+  }
+  CharacterAttributes[] defaultCharacters;
+
+  constructor(
+    string [] memory characterNames, 
+    string [] memory characterImageURIs, 
+    uint32[] memory characterHp, 
+    uint32[] memory characterAttackDmg 
+  ) 
+  {
+    for(uint i = 0 ; i < characterNames.length ; i +=1){
+      defaultCharacters.push(CharacterAttributes({
+        characterIndex: i,
+        name: characterNames[i],
+        imageURI: characterImageURIs[i],
+        hp: characterHp[i],
+        maxHp: characterHp[i],
+        attackDamage: characterAttackDmg[i]
+      }));
+     CharacterAttributes memory c = defaultCharacters[i];
+     console.log("Done init %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+
+    }
+    
+    //console.log("THIS IS MY GAME CONTRACT. NICE.");
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,13 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Watson", "Smoky", "Alienator"],       // Names
+        ["https://weird-whales.com/_next/image?url=https%3A%2F%2Fgithub.com%2Fbenyaminahmed%2Fweird-whales-images%2Fraw%2Fmain%2Foptimized-images%2F600x600%2F3087.png%3Fraw%3Dtrue&w=384&q=75", // Images
+        "https://weird-whales.com/_next/image?url=https%3A%2F%2Fgithub.com%2Fbenyaminahmed%2Fweird-whales-images%2Fraw%2Fmain%2Foptimized-images%2F600x600%2F3059.png%3Fraw%3Dtrue&w=384&q=75", 
+        "https://weird-whales.com/_next/image?url=https%3A%2F%2Fgithub.com%2Fbenyaminahmed%2Fweird-whales-images%2Fraw%2Fmain%2Foptimized-images%2F600x600%2F115.png%3Fraw%3Dtrue&w=384&q=75"],
+        [350, 150, 300],                    // HP values
+        [23, 80, 25]                       // Attack damage values
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
   };
